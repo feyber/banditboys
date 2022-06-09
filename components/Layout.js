4ec9121a832ea57a286e60325c071b92f52f843f
feyber <feyberho@gmail.com>
2022-06-09 08:18:46 +0700

    header

---
+++ components/Layout.js
@@ -0,0 +1,101 @@
+import React from 'react'
+import Head from 'next/head'
+import Image from 'next/image'
+import profilePic from '../public/logobbkuning.png'
+import magiceden from '../public/me.png'
+import opensea from '../public/opensea.png'
+import discord from '../public/discord.png'
+import staking from '../public/staking.png'
+import twitter from '../public/twitter.png'
+import rare from '../public/rare.png'
+export default function Layout({ children }) {
+  return (
+    <>
+      <Head>
+        <title>Bandit Boys DAO - NFT</title>
+        <meta
+          name='description'
+          content='LFG! We Create Best DAO in Solana Network '
+        />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <div className='flex min-h-screen flex-col justify-between'>
+        <header>
+          <nav className='flex md:justify-between w-11/12 mx-auto'>
+            <div className='py-2 md:py-6 cursor-pointer scale-75 md:scale-100'>
+              <Image
+                src={profilePic}
+                alt='Picture of the author'
+                width={100}
+                height={100}
+                className='drop-shadow-sm'
+              />
+            </div>
+            <div className='flex -mr-4 md:mr-0 py-2 md:py-6 scale-75 md:scale-100 space-x-2 md:space-x-2'>
+              <a href='https://www.magiceden.io/marketplace/bandit_boys'>
+                <Image
+                  src={magiceden}
+                  alt='Picture of the author'
+                  width={35}
+                  height={35}
+                  className='drop-shadow-sm'
+                />
+              </a>
+              <a href='https://opensea.io/collection/bandit-boys-solana'>
+                <Image
+                  src={opensea}
+                  alt='Picture of the author'
+                  width={35}
+                  height={35}
+                  className='drop-shadow-sm'
+                />
+              </a>
+
+              <a href='https://www.discord.gg/npuf3zk2uV'>
+                <Image
+                  src={discord}
+                  alt='Picture of the author'
+                  width={35}
+                  height={35}
+                  className='drop-shadow-sm'
+                />
+              </a>
+              <a href='https://www.twitter.com/BanditBoysDAO'>
+                <Image
+                  src={twitter}
+                  alt='Picture of the author'
+                  width={35}
+                  height={35}
+                  className='drop-shadow-sm'
+                />
+              </a>
+
+              <a href='https://diamondvaults.io/vault/BanditBoys'>
+                <Image
+                  src={staking}
+                  alt='Picture of the author'
+                  width={35}
+                  height={35}
+                  className='drop-shadow-sm'
+                />
+              </a>
+              <a href='https://howrare.is/banditboys'>
+                <Image
+                  src={rare}
+                  alt='Picture of the author'
+                  width={35}
+                  height={35}
+                  className='drop-shadow-sm'
+                />
+              </a>
+            </div>
+          </nav>
+        </header>
+        <main>{children}</main>
+        <footer className='text-sm md:text-2xl text-center py-2'>
+          Copyright &copy; Bandit Boys NFT 2022
+        </footer>
+      </div>
+    </>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,9 @@
-import Head from 'next/head'
-// import Image from 'next/image'
-import TextyAnim from 'rc-texty'
+import Layout from '../components/Layout'
 
 export default function Home() {
   return (
-    <div>
-      <Head>
-        <title>Bandit Boys DAO - NFT</title>
-        <meta
-          name='description'
-          content='LFG! We Create Best DAO in Solana Network '
-        />
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-
-      <main>
-        {/* <TextyAnim className='text-4xl'>Hello Bandish</TextyAnim> */}
-        <h1 className=' text-xl md:text-3xl tracking-wide font-bold uppercase p-8'>
-          &#128075; Hello #Bandish. Our website very soon! &#128513;
-        </h1>
-      </main>
-      <footer></footer>
-    </div>
+    <Layout>
+      <h1 className='text-4xl'>coba bandith font</h1>
+    </Layout>
   )
 }
