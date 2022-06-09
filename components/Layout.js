d40fb722d6ddb6ed64042cbee388ed44f3c7c668
feyber <feyberho@gmail.com>
2022-06-09 08:39:20 +0700

    smain slide

--- components/Layout.js
+++ components/Layout.js
@@ -19,78 +19,78 @@ export default function Layout({ children }) {
         />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <div className='flex min-h-screen flex-col justify-between'>
-        <header>
-          <nav className='flex md:justify-between w-11/12 mx-auto'>
-            <div className='py-2 md:py-6 cursor-pointer scale-75 md:scale-100'>
+      <header className='w-full fixed z-10 justify-between'>
+        <nav className='flex justify-between w-11/12 mx-auto'>
+          <div className='py-2 md:py-6 cursor-pointer scale-75 md:scale-100'>
+            <Image
+              src={profilePic}
+              alt='Picture of the author'
+              width={100}
+              height={100}
+              className='drop-shadow-sm'
+            />
+          </div>
+          <div className='flex py-2 md:py-6 scale-75 md:scale-100 space-x-2 md:space-x-2'>
+            <a href='https://www.magiceden.io/marketplace/bandit_boys'>
               <Image
-                src={profilePic}
+                src={magiceden}
                 alt='Picture of the author'
-                width={100}
-                height={100}
+                width={35}
+                height={35}
                 className='drop-shadow-sm'
               />
-            </div>
-            <div className='flex -mr-4 md:mr-0 py-2 md:py-6 scale-75 md:scale-100 space-x-2 md:space-x-2'>
-              <a href='https://www.magiceden.io/marketplace/bandit_boys'>
-                <Image
-                  src={magiceden}
-                  alt='Picture of the author'
-                  width={35}
-                  height={35}
-                  className='drop-shadow-sm'
-                />
-              </a>
-              <a href='https://opensea.io/collection/bandit-boys-solana'>
-                <Image
-                  src={opensea}
-                  alt='Picture of the author'
-                  width={35}
-                  height={35}
-                  className='drop-shadow-sm'
-                />
-              </a>
+            </a>
+            <a href='https://opensea.io/collection/bandit-boys-solana'>
+              <Image
+                src={opensea}
+                alt='Picture of the author'
+                width={35}
+                height={35}
+                className='drop-shadow-sm'
+              />
+            </a>
 
-              <a href='https://www.discord.gg/npuf3zk2uV'>
-                <Image
-                  src={discord}
-                  alt='Picture of the author'
-                  width={35}
-                  height={35}
-                  className='drop-shadow-sm'
-                />
-              </a>
-              <a href='https://www.twitter.com/BanditBoysDAO'>
-                <Image
-                  src={twitter}
-                  alt='Picture of the author'
-                  width={35}
-                  height={35}
-                  className='drop-shadow-sm'
-                />
-              </a>
+            <a href='https://www.discord.gg/npuf3zk2uV'>
+              <Image
+                src={discord}
+                alt='Picture of the author'
+                width={35}
+                height={35}
+                className='drop-shadow-sm'
+              />
+            </a>
+            <a href='https://www.twitter.com/BanditBoysDAO'>
+              <Image
+                src={twitter}
+                alt='Picture of the author'
+                width={35}
+                height={35}
+                className='drop-shadow-sm'
+              />
+            </a>
 
-              <a href='https://diamondvaults.io/vault/BanditBoys'>
-                <Image
-                  src={staking}
-                  alt='Picture of the author'
-                  width={35}
-                  height={35}
-                  className='drop-shadow-sm'
-                />
-              </a>
-              <a href='https://howrare.is/banditboys'>
-                <Image
-                  src={rare}
-                  alt='Picture of the author'
-                  width={35}
-                  height={35}
-                  className='drop-shadow-sm'
-                />
-              </a>
-            </div>
-          </nav>
-        </header>
+            <a href='https://diamondvaults.io/vault/BanditBoys'>
+              <Image
+                src={staking}
+                alt='Picture of the author'
+                width={35}
+                height={35}
+                className='drop-shadow-sm'
+              />
+            </a>
+            <a href='https://howrare.is/banditboys'>
+              <Image
+                src={rare}
+                alt='Picture of the author'
+                width={35}
+                height={35}
+                className='drop-shadow-sm'
+              />
+            </a>
+          </div>
+        </nav>
+      </header>
+      <div className='flex flex-col justify-between'>
         <main>{children}</main>
         <footer className='text-sm md:text-2xl text-center py-2'>
           Copyright &copy; Bandit Boys NFT 2022
